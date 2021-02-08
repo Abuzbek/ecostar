beade5805d0458e7abc1868d1171e914f4ccac7d
AbuzCoder <abuzcoder@gmail.com>
2021-02-08 08:43:36 +0500

    add eco

---
+++ client/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app>
+    <v-app-bar app color="white" height="88px">
+      <div style="width:100%;" class="d-flex align-center">
+        <img
+          alt="star Logo"
+          class="shrink mx-8"
+          style="flex: 0 0 auto; "
+          :src="require('./assets/logo.svg')"
+        >
+        <v-btn tag="a" v-for="n in buttonLeft" :key="n.name" depressed color="transparent">
+          {{ n.name }}
+        </v-btn>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <Header/>
+      <About/>
+      <Skill/>
+      <Portfolio/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Header from './components/header'
+import About from './components/about'
+import Skill from './components/section'
+import Portfolio from './components/portfolio'
+
+
+export default {
+  name: 'App',
+
+  components: {
+    Header,About,Skill,Portfolio
+  },
+
+  data: () => ({
+    buttonLeft:[
+      {
+        name:'Biz haqimizda'
+      },
+      {
+        name:'Xizmatlarimiz'
+      },
+      {
+        name:'Kontaktlarimiz'
+      },
+    ],
+  }),
+};
+</script>
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap');
+</style>
---
+++ client/src/components/portfolio.vue
@@ -0,0 +1,63 @@
+<template>
+  <section>
+    <v-container>
+      <h1>Sizga quyidagi xizmatlarni taklif qilamiz</h1>
+      <v-row>
+        <v-col cols="12" lg="3" md="4" sm="6" v-for="n in item" :key="n.img" >
+          <img :src="n.img" alt="" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
+</template>
+<script>
+export default {
+  data: () => ({
+    item: [
+      {
+        img: require("../assets/1.jpg"),
+      },
+      {
+        img: require("../assets/2.2.jpg"),
+      },
+      {
+        img: require("../assets/3.jpg"),
+      },
+      {
+        img: require("../assets/4.jpg"),
+      },
+      {
+        img: require("../assets/5.jpg"),
+      },
+      {
+        img: require("../assets/6.jpg"),
+      },
+      {
+        img: require("../assets/7.jpg"),
+      },
+      {
+        img: require("../assets/8.jpg"),
+      },
+    ],
+  }),
+};
+</script>
+<style lang="scss" scoped>
+section{
+  padding:90px 0;
+}
+h1 {
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 150%;
+  text-align: center;
+  color: #454545;
+  margin-bottom: 60px;
+}
+img {
+  width: 100%;
+  height: 100%;
+}
+</style>
